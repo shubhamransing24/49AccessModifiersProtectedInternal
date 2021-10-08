56a20df909d917a307bb3228de770abf8b8f10f4
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-08 11:39:54 +0530

    Add project files.

---
+++ 49AccessModifiersProtectedInternal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _49AccessModifiersProtectedInternal
+{
+    class Rahul
+    {
+        public  virtual void Printu()
+        {
+            Console.WriteLine("This is the rahul Printu");
+        }
+    }
+     class Shubham:Rahul
+    {
+        private static void Prints()
+        {
+            Console.WriteLine("Print Method");
+        }
+    
+        public  sealed override void Printu()
+        {
+            Console.WriteLine("Printu");
+            //Shubham.Prints();
+        }
+       
+    }
+    class Vishal : Shubham
+    {
+        public override void Printu()
+        {
+            Console.WriteLine("vishal Printu");
+        }
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Rahul s = new Shubham();
+            s.Printu();
+            Shubham r = new Vishal();
+            r.Printu();
+            
+        }
+
+    }
+    
+}
